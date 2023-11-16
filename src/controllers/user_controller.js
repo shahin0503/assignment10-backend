0d2d383e5a3aef1cf7cb08b7f7c4ddca1219da13
unknown <shahinbanu.bodeliwala.at@gmail.com>
2023-11-16 12:34:44 +0530

    update user

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -9,31 +9,53 @@ const userController = {
             const newUser = new UserModel(userData)
             await newUser.save()
 
-            return res.json({success: true, data: newUser, message: "User created!"})
-            
+            return res.json({ success: true, data: newUser, message: "User created!" })
+
         } catch (error) {
-            return res.json({ success: false, message: error})
+            return res.json({ success: false, message: error })
         }
     },
 
-    signIn: async (req,res) => {
-        try{
-            const {email, password} =  req.body
+    signIn: async (req, res) => {
+        try {
+            const { email, password } = req.body
 
-            const foundUser = await UserModel.findOne({ email: email})
-            if(!foundUser){
-                return res.json({ success: false, message: 'User not found!'})
+            const foundUser = await UserModel.findOne({ email: email })
+            if (!foundUser) {
+                return res.json({ success: false, message: 'User not found!' })
             }
 
-          const passwordsMatch = bcrypt.compareSync(password, foundUser.password)
+            const passwordsMatch = bcrypt.compareSync(password, foundUser.password)
 
-          if(!passwordsMatch){
-            return res.json({success: false, message: 'Incorrect password!'})
-          }
-          return res.json({success: true, data: foundUser})
+            if (!passwordsMatch) {
+                return res.json({ success: false, message: 'Incorrect password!' })
+            }
+            return res.json({ success: true, data: foundUser })
 
-        }catch(error){
-            return res.json({ success: false, message: error})
+        } catch (error) {
+            return res.json({ success: false, message: error })
+        }
+    },
+
+    updateUser: async function (req, res) {
+        try {
+            const userId = req.params.id
+            const updateData = req.body
+
+            const updatedUser = await UserModel.findOneAndUpdate(
+                { _id: userId },
+                updateData,
+                { new: true }
+            )
+
+            if (!updatedUser) {
+                throw 'user not found!'
+            }
+
+            return res.json({ success: true, data: updateData, message: 'User updated!' })
+
+        } catch (error) {
+            return res.json({ success: false, message: error })
         }
     }
 
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/user_controller')
 
 UserRoutes.post('/createAccount', userController.createAccount)
 UserRoutes.post('/signIn', userController.signIn)
+UserRoutes.put('/:id', userController.updateUser)
 
 module.exports = UserRoutes 
